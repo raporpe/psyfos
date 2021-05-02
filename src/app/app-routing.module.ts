c52e67b4bad6f8f6db0caa0bab9c9683a87a5eeb
Raúl P <raporpe@gmail.com>
2021-05-02 21:10:39 +0200

    Login done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: '', component: AppLayoutComponent,
     children: [
       { path: 'login', component: LoginComponent},
-      { path: 'register', component: RegisterComponent},
+      { path: 'dashboard', component: DashboardComponent},
     ]}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppLayoutComponent } from './app-layout/app-layout.component';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    AppLayoutComponent
+    AppLayoutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
