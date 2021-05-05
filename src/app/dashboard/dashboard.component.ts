173af1a5ae735abe60cd603c231f3e9a5856961b
diego <dgpmakes@gmail.com>
2021-05-05 10:39:58 +0200

    Login and minicards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,4 +12,38 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
+
+}
+
+class grupo {
+  
+  votaciones:votacion[];
+
+  constructor(votaciones:votacion[]) {
+    this.votaciones = votaciones;
+  }
+}
+
+class votacion {
+  constructor() {
+    
+  }
+}
+
+
+class miembro {
+  id:number;
+  nombre_usuario:string;
+  contraseña:string;
+  grupos:grupo[];
+  
+
+  constructor(id:number, nombre_usuario:string, contraseña:string, grupos: grupo[] ) {
+    this.id = id;
+    this.nombre_usuario = nombre_usuario;
+    this.contraseña = contraseña;
+    this.grupos = grupos;
+  }
 }
